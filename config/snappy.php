194ba5bd0612023d198d9a2783c61394748193c0
melmundo6 <melissamundo@prodooh.com>
2024-11-14 14:11:16 -0600

    [update] sizes in form

--- config/snappy.php
+++ config/snappy.php
@@ -43,7 +43,7 @@ return [
 
     'image' => [
         'enabled' => true,
-        'binary'  => '"/usr/local/bin/wkhtmltoimage"',
+        'binary'  => '"/usr/local/bin/wkhtmltoimage"',//C:\Program Files\wkhtmltopdf\bin\wkhtmltoimage.exe
         'timeout' => false,
         'options' => [],
         'env'     => [],
